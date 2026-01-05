f90d4ea24181219515a1a6a95edd26e2cef1523a
ZhangWenhao <wbfw-zwh@eastmoney.com>
2026-01-05 16:37:03 +0800

    启动项目， 移除冗余配置

---
+++ api/src/main/java/com/wendell/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.wendell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZhangWenhao
+ * @date 2026/1/5 16:32
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
